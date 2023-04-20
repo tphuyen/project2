601a8c2a7657db22cf7d51e07bff55bac21ea575
tphuyen <trinhhuyen.tph@gmail.com>
2023-04-20 07:52:24 +0700

    init repo

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+  <></>
+  );
+}
+
+export default App;
